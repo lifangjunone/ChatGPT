71a823024572f6cbc967052ef40e0a1f7bded964
lifangjun <lifj@ebondhm.com>
2023-06-30 18:18:00 +0800

    feat(persion)

--- ChatGPT/api/backend/permission.py
+++ ChatGPT/api/backend/permission.py
@@ -12,58 +12,142 @@ class PermissionViewSet(Resource):
             {
                 "children": [
                     {
-                        "children": None,
-                        "id": 2,
-                        "name": "核心技术",
-                        "parentId": 1,
-                        "sort": 1,
-                        "type": 2,
-                        "url": "/main/analysis/overview"
-                    },
-                    {
+                        "icon": "\ue658",
                         "children": None,
                         "id": 3,
                         "name": "核心技术",
                         "parentId": 1,
+                        "sort": 1,
+                        "type": 2,
+                        "url": "/main/system/core_tech"
+                    },
+                    {
+                        "icon": "\ue66c",
+                        "children": None,
+                        "id": 4,
+                        "name": "商品统计",
+                        "parentId": 1,
                         "sort": 2,
                         "type": 2,
-                        "url": "/main/analysis/overview"
+                        "url": "/main/system/shop_statistics"
                     }
                 ],
-                "icon": "el-icon-monitor",
+                "icon": "\ue612",
                 "id": 1,
                 "name": "系统总览",
                 "sort": 1,
                 "type": 1,
-                "url": "/main/analysis1"
+                "url": "/main/system"
             },
             {
                 "children": [
                     {
+                        "icon": "\ue8c8",
                         "children": None,
-                        "id": 4,
-                        "name": "核心技术",
+                        "id": 5,
+                        "name": "用户管理",
                         "parentId": 2,
                         "sort": 1,
                         "type": 2,
-                        "url": "/main/analysis/overview"
+                        "url": "/main/manage/user"
                     },
                     {
+                        "icon": "\ue608",
                         "children": None,
-                        "id": 5,
-                        "name": "核心技术",
+                        "id": 6,
+                        "name": "部门管理",
                         "parentId": 2,
-                        "sort": 2,
+                        "sort": 3,
                         "type": 2,
-                        "url": "/main/analysis/overview"
+                        "url": "/main/manage/department"
+                    },
+                    {
+                        "icon": "\ue64d",
+                        "children": None,
+                        "id": 7,
+                        "name": "菜单管理",
+                        "parentId": 2,
+                        "sort": 4,
+                        "type": 2,
+                        "url": "/main/manage/menu"
+                    },
+                    {
+                        "icon": "\ue6a0",
+                        "children": None,
+                        "id": 8,
+                        "name": "角色管理",
+                        "parentId": 2,
+                        "sort": 5,
+                        "type": 2,
+                        "url": "/main/manage/role"
                     }
                 ],
-                "icon": "el-icon-monitor",
+                "icon": "\ue617",
                 "id": 2,
-                "name": "系统总览",
+                "name": "系统管理",
                 "sort": 2,
                 "type": 1,
-                "url": "/main/analysis"
+                "url": "/main/manage"
+            },
+            {
+                "children": [
+                    {
+                        "icon": "\ue603",
+                        "children": None,
+                        "id": 10,
+                        "name": "商品类别",
+                        "parentId": 9,
+                        "sort": 1,
+                        "type": 2,
+                        "url": "/main/shop/shop_category"
+                    },
+                    {
+                        "icon": "\ue62b",
+                        "children": None,
+                        "id": 11,
+                        "name": "商品信息",
+                        "parentId": 9,
+                        "sort": 3,
+                        "type": 2,
+                        "url": "/main/shop/shop_info"
+                    }
+                ],
+                "icon": "\ue60a",
+                "id": 9,
+                "name": "商品中心",
+                "sort": 3,
+                "type": 1,
+                "url": "/main/shop"
+            },
+            {
+                "children": [
+                    {
+                        "icon": "\ueaf5",
+                        "children": None,
+                        "id": 13,
+                        "name": "你的故事",
+                        "parentId": 12,
+                        "sort": 1,
+                        "type": 2,
+                        "url": "/main/chat/your_story"
+                    },
+                    {
+                        "icon": "\ue624",
+                        "children": None,
+                        "id": 14,
+                        "name": "故事列表",
+                        "parentId": 12,
+                        "sort": 2,
+                        "type": 2,
+                        "url": "/main/chat/story_list"
+                    }
+                ],
+                "icon": "\ue638",
+                "id": 12,
+                "name": "随便聊聊",
+                "sort": 4,
+                "type": 1,
+                "url": "/main/chat"
             }
         ]
         return get_return_data(Success, data)
