66d15267c43a357ce18747ab166a9cb1a44b9ab8
lifangjun <lifj@ebondhm.com>
2023-07-04 09:49:11 +0800

    user

--- ChatGPT/api/backend/user.py
+++ ChatGPT/api/backend/user.py
@@ -58,51 +58,263 @@ class UsersViewSet(Resource):
         data = [
             {
                 "id": 1,
-                "name": "god",
+                "name": "票氏集团票少",
                 "realname": "god001",
                 "cellphone": "18812324989",
                 "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
                 "createAt": "2021-01-23: 23:34:11",
                 "updateAt": "2021-01-24: 23:34:11",
-                "role": {
-                    "id": 1,
-                    "name": "超级管理员",
-                    "intro": "所有权限",
-                    "createAt": "2021-01-23: 23:34:11",
-                    "updateAt": "2021-01-24: 23:34:11",
-                },
-                "department": {
-                    "id": 1,
-                    "name": "研发部",
-                    "parentId": None,
-                    "createAt": "2021-01-23: 23:34:11",
-                    "updateAt": "2021-01-24: 23:34:11",
-                    "leader": "god"
-                }
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
             },
             {
                 "id": 2,
-                "name": "god",
+                "name": "李氏集团李少",
                 "realname": "god002",
                 "cellphone": "18612324989",
                 "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
                 "createAt": "2021-01-23: 23:34:11",
                 "updateAt": "2021-01-24: 23:34:11",
-                "role": {
-                    "id": 1,
-                    "name": "超级管理员",
-                    "intro": "所有权限",
-                    "createAt": "2021-01-23: 23:34:11",
-                    "updateAt": "2021-01-24: 23:34:11",
-                },
-                "department": {
-                    "id": 1,
-                    "name": "研发部",
-                    "parentId": None,
-                    "createAt": "2021-01-23: 23:34:11",
-                    "updateAt": "2021-01-24: 23:34:11",
-                    "leader": "god"
-                }
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 3,
+                "name": "刘氏集团刘少",
+                "realname": "god003",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 4,
+                "name": "策氏集团策少",
+                "realname": "god004",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 5,
+                "name": "王氏集团王少",
+                "realname": "god005",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 6,
+                "name": "向氏集团向少",
+                "realname": "god006",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 7,
+                "name": "杨氏集团杨少",
+                "realname": "god007",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 8,
+                "name": "高氏集团高少",
+                "realname": "god008",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 9,
+                "name": "叶氏集团叶少",
+                "realname": "god009",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
+            },
+            {
+                "id": 10,
+                "name": "俞氏集团俞少",
+                "realname": "god010",
+                "cellphone": "18612324989",
+                "enable": 1,
+                "department_Id": 1,
+                "roleId": 1,
+                "createAt": "2021-01-23: 23:34:11",
+                "updateAt": "2021-01-24: 23:34:11",
+                # "role": {
+                #     "id": 1,
+                #     "name": "超级管理员",
+                #     "intro": "所有权限",
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                # },
+                # "department": {
+                #     "id": 1,
+                #     "name": "研发部",
+                #     "parentId": None,
+                #     "createAt": "2021-01-23: 23:34:11",
+                #     "updateAt": "2021-01-24: 23:34:11",
+                #     "leader": "god"
+                # }
             }
         ]
         return_data = {
